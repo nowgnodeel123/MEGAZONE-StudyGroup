19f69b706e9733fb0ea62543cb0808f674154cb5
nowgnodeel123 <nowgnodeel123@gmail.com>
2024-07-02 20:53:04 +0900

    second commit

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+j
+:src/main/java/com/megazone/recruit/RecruitApplication.java,f\7\f7bb464c8a38a05827dcd372b2fecdd993d2a2aa
--- src/main/java/com/megazone/recruit/RecruitApplication.java
+++ src/main/java/com/megazone/recruit/RecruitApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecruitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecruitApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RecruitApplication.class, args);
+  }
 
 }
